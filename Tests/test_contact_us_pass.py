8450d59037fcf3fa059f238063beac7e383e92ef
aharutyunova <anna.harutyunova@volo.global>
2021-02-22 02:54:46 +0400

    Changes by Anna

---
+++ Tests/test_contact_us_pass.py
@@ -0,0 +1,65 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import json
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+import os
+import sys
+from lib import LIB
+from Pages.home import Home
+from Pages.sign_in import Sign_In
+from Pages.contact_us import Contact_Us
+import pytest
+
+def test_ContuctUs_pass():
+    try:
+## 1. Go to URL
+        object_lib = LIB()
+        browser = object_lib.OpenBrowser()
+        object_lib.PageLoad(browser)
+
+# 2. Click to Contact US sign_in_button
+        object_home = Home(browser)
+        contact_us_page = object_home.ContactUs(browser)
+
+##browser.find_element(By.CSS_SELECTOR, "div#contact-link a").send_keys(Keys.ENTER)
+
+# 3. Fill all fields in Contact Us and click on Send button
+        object_contactus = Contact_Us(browser)
+        object_contactus.FillFeilds(browser)
+
+        object_contactus.FillText(browser)
+
+        object_contactus.ClickSend(browser)
+
+# 4. Validate that success message displayed
+        assert "Your message has been successfully sent to our team." in browser.page_source
+# 5. Close browser
+
+        print("Test run Passed!")
+        
+    # except Exception as e:
+    #     print(e)
+    #     print("Test run Failed")
+
+    finally:
+        object_lib.CloseBrowser(browser)
+
+test_ContuctUs_pass()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
